825de4bc70a932ab2725af0b3a8b81109d46fb6a
Valentina Correa <vcorrea@fi.uba.ar>
2023-09-20 19:00:35 -0300

    server now also receives metadata

--- TP1/src/start-server.py
+++ TP1/src/start-server.py
@@ -6,10 +6,24 @@ path = 'test.txt'
 # CONSTANTES
 CHUNK_SIZE = 64
 
-with open('lib/server-files/' + path, "wb") as f:
-    with socket(AF_INET, SOCK_DGRAM) as s:
-        s.bind(('', serverPort))
-        chunk, clientAddress = s.recvfrom(CHUNK_SIZE)
-        f.write(chunk)
 
+with socket(AF_INET, SOCK_DGRAM) as s:
+    s.bind(('', serverPort))
+    content, clientAddress = s.recvfrom(CHUNK_SIZE)
+
+    fileSize = int.from_bytes(content[:8], byteorder='big')
+    fileName = content[8:].decode()
+
+    print("FILE SIZE:", fileSize)
+    print("FILE NAME:", fileName)
+
+    received = 0
+    with open('lib/server-files/' + fileName, "wb") as f:
+        while received < fileSize:
+            content, clientAddress = s.recvfrom(CHUNK_SIZE)
+            received += len(content)
+            print(content)
+            f.write(content)
+
+s.close()
 f.close()
--- TP1/src/upload.py
+++ TP1/src/upload.py
@@ -1,3 +1,4 @@
+import os
 from socket import *
 
 serverName = '127.0.0.1'
@@ -9,6 +10,16 @@ CHUNK_SIZE = 64
 
 with open('lib/client-files/' + path, "rb") as f:
     with socket(AF_INET, SOCK_DGRAM) as s:
+        # PRIMER PAQUETE DE METADATA
+        # [ LARGO DEL ARCHIVO en bytes ] 64 bits
+        # [ NOMBRE                     ] 64 bits
+        # ...
+        # [ NOMBRE                     ] 64 bits
+
+        fileSize = os.stat('lib/client-files/' + path).st_size.to_bytes(8, byteorder='big')
+        metadata = fileSize + path.encode()
+        s.sendto(metadata, (serverName, serverPort))
+
         while chunk := f.read(CHUNK_SIZE):
             s.sendto(chunk, (serverName, serverPort))
 
