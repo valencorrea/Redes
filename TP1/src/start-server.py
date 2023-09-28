55871fbe15de792f4e9b2b3c604a86fed3439428
Juan S. Burgos <jsburgos@fi.uba.ar>
2023-09-28 00:30:10 -0300

    Control de paquetes en orden, tamaño variable de archivo

--- TP1/src/start-server.py
+++ TP1/src/start-server.py
@@ -1,45 +1,51 @@
 from socket import *
+from constants import *
 
 serverPort = 12001
 path = 'correa-tp-greedy.txt'
 serverName = '127.0.0.1'
 
 
-# CONSTANTES
-CHUNK_SIZE = 128
-
 with socket(AF_INET, SOCK_DGRAM) as s:
-    s.bind(('', serverPort))
-    content, clientAddress = s.recvfrom(CHUNK_SIZE)
 
-    fileSize = int.from_bytes(content[:8], byteorder='big')
-    fileName = content[8:].decode()
+    s.bind(('', serverPort))
+    print("Socket open at:", serverPort)
+    package, clientAddress = s.recvfrom(CHUNK_SIZE)
+    print("recv handshake")
+    package_id = int.from_bytes(package[:HEADER_SIZE])
+    fileSize = int.from_bytes(package[HEADER_SIZE:HEADER_SIZE + LENGTH_BYTES])
+    fileName = package[HEADER_SIZE + LENGTH_BYTES:].decode('utf-8')
+    fileName = fileName.rstrip('\0')
+    print("File name received:", fileName, 'of size:', fileSize)
+
+    if fileSize > 5000000:  # Hacer las validaciones de verdad
+        res = package_id.to_bytes(HEADER_SIZE) + FILE_TOO_BIG.to_bytes(STATUS_CODE_SIZE)
+        print("File too big")
+    else:
+        res = package_id.to_bytes(HEADER_SIZE) + STATUS_OK.to_bytes(STATUS_CODE_SIZE)
+        print("Status OK")
+    print("About to send response")
+    s.sendto(res, clientAddress)
+    print("response sent")
 
     received = 0
-    oldId = -1
+    oldId = package_id
 
     with open('lib/server-files/' + fileName, "wb") as f:
         while received < fileSize:
-            content, clientAddress = s.recvfrom(CHUNK_SIZE)
+            if fileSize - received < CHUNK_SIZE - HEADER_SIZE:
+                package, clientAddress = s.recvfrom(CHUNK_SIZE)
+            else:
+                package, clientAddress = s.recvfrom(fileSize - received + HEADER_SIZE)
+            package_id = int.from_bytes(package[:HEADER_SIZE])
+            data = package[HEADER_SIZE:]
 
-            id, ack, chunk = int.from_bytes(content[:2], byteorder='big'), int.from_bytes(content[2:10], byteorder='big'), content[16:]
-
-            if oldId == id:
-                responseAck = id
-                chunkMetadata = oldId.to_bytes(2, byteorder='big') + responseAck.to_bytes(8, byteorder='big')
-                data = chunkMetadata + chunk
-                s.sendto(data, (clientAddress[0], clientAddress[1]))
-                continue
-
-            received += len(chunk)
-            f.write(chunk)
-            oldId = id
-
-            responseAck = id
-            chunkMetadata = oldId.to_bytes(2, byteorder='big') + responseAck.to_bytes(8, byteorder='big')
-            data = chunkMetadata + chunk
-            s.sendto(data, (clientAddress[0], clientAddress[1]))
-
+            if (oldId + 1) == package_id:  # Recibi paquete siguiente
+                received += len(data)
+                f.write(data)
+                oldId = package_id
+            res = oldId.to_bytes(HEADER_SIZE)
+            s.sendto(res, clientAddress)
 
 s.close()
 f.close()
--- TP1/src/upload.py
+++ TP1/src/upload.py
@@ -1,45 +1,59 @@
 import os
-import sys
+from constants import *
 from socket import *
 
 serverName = '127.0.0.1'
 serverPort = 12001
-path = 'correa-tp-greedy.txt'
-
-# CONSTANTES
-CHUNK_SIZE = 128
-TIMEOUT = 5 # RANDOM, PERO PONER ALGUNA JUSTIFICACION EN EL INFORME
-
-class header:
-    def __init__(self, seqNumber, ack):
-        self.seqNumber = seqNumber
-        self.ack = ack
+fileName = 'correa-tp-greedy.txt'
 
 
-with open('lib/client-files/' + path, "rb") as f:
+with (open('lib/client-files/' + fileName, "rb") as f):
     with socket(AF_INET, SOCK_DGRAM) as s:
-        # PRIMER PAQUETE DE METADATA
-        # [ LARGO DEL ARCHIVO en bytes ] 64 bits
-        # [ NOMBRE                     ] 64 bits
-        # ...
-        # [ NOMBRE                     ] 64 bits
+        print("Socket created")
         s.settimeout(TIMEOUT)
-        fileSize = os.stat('lib/client-files/' + path).st_size.to_bytes(8, byteorder='big')
-        fileMetadata = fileSize + path.encode()
-        s.sendto(fileMetadata, (serverName, serverPort))
+        package_id = 0
 
-        id = 0
-        ack = 0
+        # PRIMER PAQUETE DE PETICION
+        # [ ID (Paquete 0) ] 1 byte
+        # [ LARGO DEL ARCHIVO en bytes ] 8 bytes
+        # [ NOMBRE                     ] 8 bytes
+        # ...
+        # [ NOMBRE                     ] 8 bytes
 
-        while chunk := f.read(CHUNK_SIZE-16):
-            chunkMetadata = id.to_bytes(2, byteorder='big') + ack.to_bytes(8, byteorder='big')
-            data = chunkMetadata + chunk
-            s.sendto(data, (serverName, serverPort))
+        header = package_id.to_bytes(HEADER_SIZE)
+        fileSize = os.stat('lib/client-files/' + fileName).st_size
+        handshake = header + fileSize.to_bytes(LENGTH_BYTES) + fileName.ljust(CHUNK_SIZE - HEADER_SIZE - LENGTH_BYTES, '\0').encode('utf-8')
+        print(len(handshake))
 
-            content, clientAddress = s.recvfrom(CHUNK_SIZE)
-            id, ack, chunk = int.from_bytes(content[:2], byteorder='big'), int.from_bytes(content[2:10], byteorder='big'), content[16:]
+        print("To send file:", fileName, ' of size:', fileSize)
+        s.sendto(handshake, (serverName, serverPort))
+        print("Sent file:", fileName, ' of size:', fileSize)
+        # ACCEPT/REJECT
+        package, serverAddress = s.recvfrom(HEADER_SIZE + STATUS_CODE_SIZE)  # serverAddress: ('123.0.8.0', 55555)
+        ack = int.from_bytes(package[:HEADER_SIZE])
+        status_code = int.from_bytes(package[HEADER_SIZE:HEADER_SIZE + STATUS_CODE_SIZE])
 
-            if ack == id:
-                id = id + 1
+        if status_code is not STATUS_OK:
+            print("El servidor respondió con error", status_code)
+            exit(1)
+        else:
+            print("Status OK")
+        package_id += 1
+
+        # ENVIO PAQUETES DE DATOS
+        # [ ID (numero del paquete)    ] 1 byte
+        # [           DATA             ] 255 bytes
+
+        while data := f.read(CHUNK_SIZE - HEADER_SIZE):
+            header_b = package_id.to_bytes(HEADER_SIZE)
+            chunk = header_b + data
+            s.sendto(chunk, serverAddress)
+
+            package, serverAddress = s.recvfrom(HEADER_SIZE)
+            ack = int.from_bytes(package)
+            print(ack)
+            if ack == package_id:
+                package_id += 1
 
 f.close()
+s.close()
