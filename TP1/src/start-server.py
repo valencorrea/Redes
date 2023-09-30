0aeb5f10284cc83e31829148cd5a0f8616788a5b
Valentina Correa <vcorrea@fi.uba.ar>
2023-09-30 15:01:31 -0300

    add while true

--- TP1/src/start-server.py
+++ TP1/src/start-server.py
@@ -7,31 +7,31 @@ serverPort = 12001
 path = 'correa-tp-greedy.txt'
 serverName = '127.0.0.1'
 
+
 def main():
-    #parser = argparse.ArgumentParser(description="Start a server.")
+    # parser = argparse.ArgumentParser(description="Start a server.")
 
-    #parser.add_argument('-H', '--host', help="Service IP address", required=True)
-    #parser.add_argument('-p', '--port', help="Service port", type=int, required=True)
-    #parser.add_argument('-s', '--storage', help="Storage directory path", required=True)
+    # parser.add_argument('-H', '--host', help="Service IP address", required=True)
+    # parser.add_argument('-p', '--port', help="Service port", type=int, required=True)
+    # parser.add_argument('-s', '--storage', help="Storage directory path", required=True)
 
-    #group = parser.add_mutually_exclusive_group()
-    #group.add_argument('-v', '--verbose', help="Increase output verbosity", action="store_true")
-    #group.add_argument('-q', '--quiet', help="Decrease output verbosity", action="store_true")
+    # group = parser.add_mutually_exclusive_group()
+    # group.add_argument('-v', '--verbose', help="Increase output verbosity", action="store_true")
+    # group.add_argument('-q', '--quiet', help="Decrease output verbosity", action="store_true")
 
-    #args = parser.parse_args()
+    # args = parser.parse_args()
     upload()
 
 
 def upload():
     with socket(AF_INET, SOCK_DGRAM) as s:
-
         s.bind(('', serverPort))
         print("Socket open at:", serverPort)
-        #llega coneccion
+        # llega conexion
         while True:
             package, clientAddress = s.recvfrom(CHUNK_SIZE)
 
-            #hago validaciones
+            # hago validaciones
             print("recv handshake")
             package_id = int.from_bytes(package[:HEADER_SIZE], byteorder='big')
             fileSize = int.from_bytes(package[HEADER_SIZE:HEADER_SIZE + LENGTH_BYTES], byteorder='big')
@@ -39,22 +39,21 @@ def upload():
             fileName = fileName.rstrip('\0')
             print("File name received:", fileName, 'of size:', fileSize)
 
-    #        if fileSize > 5000000:  # Hacer las validaciones de verdad
-    #            res = package_id.to_bytes(HEADER_SIZE) + FILE_TOO_BIG.to_bytes(STATUS_CODE_SIZE)
-    #            print("File too big")
-    #        else:
+            #        if fileSize > 5000000:  # Hacer las validaciones de verdad
+            #            res = package_id.to_bytes(HEADER_SIZE) + FILE_TOO_BIG.to_bytes(STATUS_CODE_SIZE)
+            #            print("File too big")
+            #        else:
             # puerto server, puerto cliente, puerto server donde se hace la transferencia
             # levanto hilo con port
-            res = package_id.to_bytes(HEADER_SIZE, byteorder='big') + STATUS_OK.to_bytes(STATUS_CODE_SIZE, byteorder='big')
-    #            print("Status OK")
+            res = package_id.to_bytes(HEADER_SIZE, byteorder='big') + STATUS_OK.to_bytes(STATUS_CODE_SIZE,
+                                                                                         byteorder='big')
+            #            print("Status OK")
 
             print("About to send response")
             thread = threading.Thread(target=handleChunks, args=(res, clientAddress, fileName, fileSize))
             thread.start()
 
-
     s.close()
-
 
 
 def handleChunks(res, clientAddress, fileName, fileSize):
@@ -72,14 +71,16 @@ def handleChunks(res, clientAddress, fileName, fileSize):
         received = 0
         oldId = 0
         with open('lib/server-files/' + fileName, "wb") as f:
-            while received < fileSize:
+            while received < fileSize:  # contemplar perdida de paquetes
                 package, clientAddress = transferSocket.recvfrom(CHUNK_SIZE)
                 package_id = int.from_bytes(package[:HEADER_SIZE], byteorder='big')
                 data = package[HEADER_SIZE:]
                 print(f'{oldId} != {package_id}')
                 if (oldId + 1) == package_id:  # Recibi paquete siguiente
+                    print("recibi id" + str(package_id))
                     received += len(data)
-                    f.write(data)
+                    w = f.write(data)
+                    print(w)
                     oldId = package_id
                 res = oldId.to_bytes(HEADER_SIZE, byteorder='big')
                 transferSocket.sendto(res, clientAddress)
@@ -87,5 +88,6 @@ def handleChunks(res, clientAddress, fileName, fileSize):
         f.close()
 
     transferSocket.close()
+
 
 main()
