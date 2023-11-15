8a40fd65b89e5157598c4937b537da8921766ed7
Valentina Correa <vcorrea@fi.uba.ar>
2023-11-14 22:07:28 -0300

    add controller

--- TP2/controller.py
+++ TP2/controller.py
@@ -1,36 +1,69 @@
-from pox.core import core
-import pox.openflow.libopenflow_01 as of
-from pox.lib.revent import *
-from pox.lib.util import dpidToStr
-from pox.lib.addresses import EthAddr
-from collections import namedtuple
+from pox.pox.core import core
+import pox.pox.openflow.libopenflow_01 as of
+from pox.pox.lib.revent import *
+from pox.pox.lib.revent.revent import EventMixin
+from pox.pox.lib.util import dpidToStr
+from pox.pox.lib.addresses import EthAddr
 import os
+import json
+import pox.pox.lib.packet as packet
+
 ''' Add your imports here ... '''
 
-
-
 log = core.getLogger()
-policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ[ 'HOME' ]
+policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ['HOME']
 
 ''' Add your global variables here ... '''
 
 
+class Firewall(EventMixin):
 
-class Firewall (EventMixin):
-
-    def __init__ (self):
+    def __init__(self):
         self.listenTo(core.openflow)
         config = self.read_config("rules.json")
+        self.firewall_switch_id = config["firewall_switch_id"]
         self.rules = config["rules"]
+        log.debug("Enabling Firewall Module")
 
-    def _handle_ConnectionUp (self, event):
-        ''' Add your logic here ... '''
-
-
+    def _handle_ConnectionUp(self, event):
+        if event.dpid == self.firewall_switch_id:
+            for rule in self.rules:
+                self.add_rule(event, rule["rule"])
 
         log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
 
-def launch ():
+    def add_rule(self, event, rule):
+        match = of.ofp_match()
+        if "transport" in rule:
+            self.add_transport_rule(rule["transport"], match)
+        if "network" in rule:
+            self.add_network_rule(rule["network"], match)
+
+        msg = of.ofp_flow_mod()
+        msg.match = match
+        event.connection.send(msg)
+
+    def read_config(self, file):
+        file = open(file, "r")
+        config = json.loads(file.read())
+        file.close()
+        return config
+
+    def add_transport_rule(self, rule, match):
+        if "src_port" in rule:
+            match.tp_src = rule["src_port"]
+        if "dst_port" in rule:
+            match.tp_dst = rule["dst_port"]
+
+    def add_network_rule(self, rule, match):
+        if "ip_type" in rule:
+            if "ipv4" == rule["ip_type"]:
+                match.dl_type = packet.ethernet.IP_TYPE
+            if "ipv6" == rule["ip_type"]:
+                match.dl_type = packet.ethernet.IPV6_TYPE
+
+
+def launch():
     '''
     Starting the Firewall module
     '''
--- TP2/topology.py
+++ TP2/topology.py
@@ -5,23 +5,38 @@ class MyTopo(Topo):
     def __init__(self):
         Topo.__init__(self)
 
-        # if number_switches < 1:
+        #if number_switches < 1:
         #    return #levantar excepcion
 
-        s1 = self.addSwitch("switch_1")
+        #s1 = self.addSwitch("switch_1")
 
-        # for i in range(0, switches):
-        #    switches.append(self.addSwitch("switch_1"))
-
-        # Create hosts
+        switches_list = []
+        for i in range (0,2):#n
+            self.addSwitch(f'switch_{i}')
         h1 = self.addHost("host_1")
         h2 = self.addHost("host_2")
+        h3 = self.addHost("host_3")
+        h4 = self.addHost("host_4")
+
+        self.addLink(switches_list[0],h1)
+        self.addLink(switches_list[0],h2)
+
+        self.addLink(switches_list[-1],h4)
+        self.addLink(switches_list[-1],h3)
+        for i in range(1,len(switches_list)):
+           self.adLink(switches_list[i-1],switches_list[i])
+
+
+
+        # Create hosts
+#        h1 = self.addHost("host_1")
+#        h2 = self.addHost("host_2")
         #        h3 = self.addHost("host_3")
         #        h3 = self.addHost("host_4")
 
         # Add links between switches and hosts self
-        self.addLink(s1, h1)
-        self.addLink(s1, h2)
+ #       self.addLink(s1, h1)
+ #       self.addLink(s1, h2)
 
         # self.addLink(s2, h3)
         # self.addLink(s2, h4)
