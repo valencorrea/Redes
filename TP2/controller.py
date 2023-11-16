8b914221122078522706ce57a33462d477d584af
Valentina Correa <vcorrea@fi.uba.ar>
2023-11-15 23:16:35 -0300

    finish controller

--- TP2/controller.py
+++ TP2/controller.py
@@ -1,12 +1,10 @@
-from pox.pox.core import core
-import pox.pox.openflow.libopenflow_01 as of
-from pox.pox.lib.revent import *
-from pox.pox.lib.revent.revent import EventMixin
-from pox.pox.lib.util import dpidToStr
-from pox.pox.lib.addresses import EthAddr
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.revent.revent import EventMixin
+from pox.lib.util import dpidToStr
+from pox.lib.addresses import EthAddr
 import os
 import json
-import pox.pox.lib.packet as packet
 
 ''' Add your imports here ... '''
 
@@ -20,24 +18,30 @@ class Firewall(EventMixin):
 
     def __init__(self):
         self.listenTo(core.openflow)
-        config = self.read_config("rules.json")
+        config = self.read_config("/home/valen/redes/Redes/TP2/rules.json")
         self.firewall_switch_id = config["firewall_switch_id"]
         self.rules = config["rules"]
         log.debug("Enabling Firewall Module")
 
-    def _handle_ConnectionUp(self, event):
+    def _handle_ConnectionUp(self, event):  # -> ver
         if event.dpid == self.firewall_switch_id:
             for rule in self.rules:
-                self.add_rule(event, rule["rule"])
+                self.add_rule(event, ["rule"])
 
         log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
 
     def add_rule(self, event, rule):
         match = of.ofp_match()
-        if "transport" in rule:
-            self.add_transport_rule(rule["transport"], match)
-        if "network" in rule:
-            self.add_network_rule(rule["network"], match)
+        log.debug("Adding firewall rule")
+
+        if "dst_port" in rule:
+            match.tp_dst = rule["dst_port"]
+        if "protocol" in rule:
+            match.nw_proto = rule["protocol"]
+        if "dl_src" in rule["dl_src"]:
+            match.dl_src = EthAddr(rule["dl_src"])
+        if "dl_dst" in rule["dl_dst"]:
+            match.dl_dst = EthAddr(rule["dl_dst"])
 
         msg = of.ofp_flow_mod()
         msg.match = match
@@ -49,22 +53,9 @@ class Firewall(EventMixin):
         file.close()
         return config
 
-    def add_transport_rule(self, rule, match):
-        if "src_port" in rule:
-            match.tp_src = rule["src_port"]
-        if "dst_port" in rule:
-            match.tp_dst = rule["dst_port"]
-
-    def add_network_rule(self, rule, match):
-        if "ip_type" in rule:
-            if "ipv4" == rule["ip_type"]:
-                match.dl_type = packet.ethernet.IP_TYPE
-            if "ipv6" == rule["ip_type"]:
-                match.dl_type = packet.ethernet.IPV6_TYPE
-
 
 def launch():
-    '''
+    """
     Starting the Firewall module
-    '''
+    """
     core.registerNew(Firewall)
--- TP2/topology.py
+++ TP2/topology.py
@@ -1,45 +1,33 @@
+import logging
+
 from mininet.topo import Topo
 
 
 class MyTopo(Topo):
-    def __init__(self):
+    def __init__(self, switches):
         Topo.__init__(self)
 
-        #if number_switches < 1:
-        #    return #levantar excepcion
-
-        #s1 = self.addSwitch("switch_1")
+        if switches < 1:
+            raise Exception("La cantidad de switches tiene que ser mayor a 0")
 
         switches_list = []
-        for i in range (0,2):#n
-            self.addSwitch(f'switch_{i}')
+        for switch in range(0, switches):
+            switches_list.append(self.addSwitch(f'switch_{switch}'))
+
         h1 = self.addHost("host_1")
         h2 = self.addHost("host_2")
         h3 = self.addHost("host_3")
         h4 = self.addHost("host_4")
 
-        self.addLink(switches_list[0],h1)
-        self.addLink(switches_list[0],h2)
+        self.addLink(switches_list[0], h1)
+        self.addLink(switches_list[0], h2)
 
-        self.addLink(switches_list[-1],h4)
-        self.addLink(switches_list[-1],h3)
-        for i in range(1,len(switches_list)):
-           self.adLink(switches_list[i-1],switches_list[i])
+        self.addLink(switches_list[-1], h4)
+        self.addLink(switches_list[-1], h3)
 
-
-
-        # Create hosts
-#        h1 = self.addHost("host_1")
-#        h2 = self.addHost("host_2")
-        #        h3 = self.addHost("host_3")
-        #        h3 = self.addHost("host_4")
-
-        # Add links between switches and hosts self
- #       self.addLink(s1, h1)
- #       self.addLink(s1, h2)
-
-        # self.addLink(s2, h3)
-        # self.addLink(s2, h4)
+        for i in range(1, len(switches_list)):
+            logging.log(2, 'addLink. switches len:' + str(len(switches_list)) + 'i:' + str(i) + 'i - 1:' + str(i - 1))
+            self.addLink(switches_list[i - 1], switches_list[i])
 
 
 topos = {'customTopo': MyTopo}
