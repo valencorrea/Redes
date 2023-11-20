a992ad7558eeda34a45bb44039f6e2421b0294ac
Valentina Correa <vcorrea@fi.uba.ar>
2023-11-20 17:59:25 -0300

    syntax fixes

--- TP2/topology.py
+++ TP2/topology.py
@@ -2,7 +2,6 @@ import logging
 
 from mininet.topo import Topo
 
-
 class MyTopo(Topo):
     def __init__(self, switches):
         Topo.__init__(self)
@@ -13,6 +12,7 @@ class MyTopo(Topo):
         switches_list = []
         for switch in range(0, switches):
             switches_list.append(self.addSwitch(f'switch_{switch}'))
+            logging.debug(f'Added switch switch_{switch}')
 
         h1 = self.addHost("host_1")
         h2 = self.addHost("host_2")
@@ -26,7 +26,7 @@ class MyTopo(Topo):
         self.addLink(switches_list[-1], h3)
 
         for i in range(1, len(switches_list)):
-            logging.log(2, 'addLink. switches len:' + str(len(switches_list)) + 'i:' + str(i) + 'i - 1:' + str(i - 1))
+            logging.debug(2, 'addLink. switches len:' + str(len(switches_list)) + 'i:' + str(i) + 'i - 1:' + str(i - 1))
             self.addLink(switches_list[i - 1], switches_list[i])
 
 
